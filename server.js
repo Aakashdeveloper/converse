9853ade752d346359ef0d5d726480d3fe858d279
aakashDeveloper <ahanda205@gmail.com>
2019-02-26 12:15:15 +0530

    a

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = '6789';
+const port = '3000';
 
-app.get('/', (req,res) => {
-    res.send({"response":'please send query'})
+app.get('/', function(req,res){
+    res.send({"response":'please send query'});
 })
 
-app.get('/converse/hi', (req,res) => {
-    res.send({"response":'hello welcome to fin bot'})
+app.get('/converse/hi', function(req,res){
+    res.send({"response":'hello welcome to fin bot'});
     
 })
-app.get('/converse/need%20access', (req,res) => {
-    fs.readFile('response1.txt',(err,data)=> {
+app.get('/converse/need%20access', function(req,res) {
+    fs.readFile('response1.txt',function(err,data){
+        if(err) throw err;
         res.send(data)
     })
 })
-app.get('/converse/needaccess', (req,res) => {
-    fs.readFile('response1.txt',(err,data)=> {
+app.get('/converse/needaccess', function(req,res){
+    fs.readFile('response1.txt',function(err,data){
         if(err) throw err;
         res.send(JSON.parse(data));
     })
 })
 
-app.listen(port,(err)=>{
+app.listen(port,function(err){
     if(err) throw err;
-    console.log(`Server is running on port ${port}`);
+    console.log("Server is running on port "+ port );
 })
